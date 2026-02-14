647763d6a38cea963afeef6ac299ebe3a7b07d12
pengyu <pengyuchen01@gmail.com>
2026-02-13 23:22:05 -0500

    adding nav bar

---
+++ frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,18 @@
+
+<template>
+ <van-nav-bar
+  title="标题"
+  left-text="返回"
+  right-text="按钮"
+  left-arrow
+  @click-left="onClickLeft"
+  @click-right="onClickRight"
+/>
+
+</template>
+
+<script setup lang="ts">
+
+    const onClickLeft = () => alert('返回');
+    const onClickRight = () => alert('按钮');
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Button, NavBar } from 'vant';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Button);
+app.use(NavBar);
+app.mount('#app')
