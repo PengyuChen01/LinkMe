412735cfd82c4b352fdc6e9da56a2bf3a4d72876
pengyu <pengyuchen01@gmail.com>
2026-02-16 23:02:57 -0500

    adding mybatis plus

---
+++ backend/src/main/java/generator/mapper/TagMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.Tag;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author pengy
+* @description 针对表【tag(标签)】的数据库操作Mapper
+* @createDate 2026-02-16 23:02:16
+* @Entity generator.domain.Tag
+*/
+public interface TagMapper extends BaseMapper<Tag> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/generator/service/TagService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.Tag;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author pengy
+* @description 针对表【tag(标签)】的数据库操作Service
+* @createDate 2026-02-16 23:02:16
+*/
+public interface TagService extends IService<Tag> {
+
+}
